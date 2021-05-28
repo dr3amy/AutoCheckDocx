c4482074f59a2087d5e522463799ecfe11310bf3
Nikita Kaftanov <arterterone@gmail.com>
2021-05-29 02:22:31 +0300

    minor refactoring

--- classes.py
+++ classes.py
@@ -1,16 +1,16 @@
 class Heading(object):
     """Класс для хранения раздела"""
 
-    def __init__(self, head, text_type, content=[], parent=None):
+    def __init__(self, head, head_type, content=[], parent=None):
         """Коснтруктор"""
 
         self.head = head
-        self.text_type = text_type
+        self.head_type = head_type
         self.content = content
         self.parent = parent
 
     def __str__(self):
-        return f"<{self.head}, '{self.text_type}', {self.content}>"
+        return f"<{self.head}, '{self.head_type}', {self.content}>"
 
     def __repr__(self):
-        return f"<{self.head}, '{self.text_type}', {self.content}>"
+        return f"<{self.head}, '{self.head_type}', {self.content}>"
