c3a9128a21bb6c9ed980e32c674ee2922c5fa26b
Nikita Kaftanov <arterterone@gmail.com>
2021-06-13 22:13:17 +0300

    minor fixes

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ from parse import Parser
 
 
 # heads_check = {
-#     'missing partitions': 0,
-#     'number of unordered partitions': 0,
+#     'sections presence': 1.0,
+#     'ordered sections': 1.0,
 #     'Введение <-> Введение': 1.0,
 #     'Организация выполнения <-> Организация выполнения': 1.0,
 #     'Анализ рисков <-> Анализ рисков': 1.0,
@@ -15,15 +15,32 @@ from parse import Parser
 #     'Механизмы мониторинга и контроля <-> Механизмы мониторинга и контроля': 1.0
 #     }
 # bodies_check = {
-#     'body #1': 0.23,
-#     'body #2': 0.23,
-#     'body #3': 0.12,
-#     'body #4': 0.12,
-#     'body #5': 0.31,
-#     'body #6': 0.44,
-#     'body #7': 0.15
+#     'body #1': 0.1,
+#     'body #2': 0.1,
+#     'body #3': 0.1,
+#     'body #4': 0.1,
+#     'body #5': 0.1,
+#     'body #6': 0.1,
+#     'body #7': 0.1
 # }
 # res = calc_result(heads_check, bodies_check)
 # print(res)
 
-test()
+# test()
+test_list = ["Введение", "Необходимые ресурсы", "Организация выполнения"]
+sort_order = ["Введение", "Организация выполнения", "Анализ рисков", "Необходимые ресурсы"]
+res = ["dummmy"] * len(sort_order)
+# res = [elem for x in sort_order for elem in test_list if elem == x]
+
+# for x in sort_order:
+#     for elem in test_list:
+#         if elem == x:
+#             res[i] = elem
+i = 0
+for elem in test_list:
+    i = sort_order.index(elem)
+    res[i] = elem
+
+
+print(test_list)
+print(res)
